322b144294070aa23c6e85a107eb809516a6cba8
annieha04 <itsannha@gmail.com>
2024-03-28 00:06:45 -0400

    imported authenticate and signupform

--- src/ components/SignUpForm.jsx
+++ src/ components/SignUpForm.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
 export default function SignUpForm() {
-    return <h2>Sign Up!</h2>;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Hello 👋");
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
+            const result = await response.json();
+            console.log(result);
+
+            setToken(result.token);
+        } catch (error) {
+            setError(error.message);
+        }
+      } 
+
+    return (
+        <div>
+            <h2>Sign Up!</h2>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <label> Username: <input /></label> 
+                <label> Password: 
+                    <input 
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <button>Submit</button>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} />
+            </form>
+        </div>
+    );
   }
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Authenticate from './ components/Authenticate';
+import SignUpForm from './ components/SignUpForm';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(null);
 
   return (
     <>
-  
+     <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
